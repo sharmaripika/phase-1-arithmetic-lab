0c21f790af401618de7c57f5fc3e3c2de0b25df2
Ripika Sharma <ripikasharma@gmail.com>
2023-08-22 16:56:56 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random(1)*10)+1;
+
+let num3 = 29;
+let num4 = 5;
+const mod = (num3%num4);
+
+let max = (1,3,6, 17,20);
+Math.max;
